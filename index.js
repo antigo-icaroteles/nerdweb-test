1e9a7b973bf786b2044250aece987cd471366aef
icarobteles <eng.icaroteles@gmail.com>
2022-11-07 20:34:59 -0300

    feat: :sparkles: add functions to open/close menu and dropdown

--- index.js
+++ index.js
@@ -20,3 +20,31 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".carousell--navigation__prev",
   },
 });
+
+const openMenuButton = document.getElementById("btnOpenMenu");
+const closeMenuButton = document.getElementById("btnCloseMenu");
+const dropdownButton = document.getElementById("btnDropdown");
+
+const openMenu = () => {
+  const menu = document.getElementById("menu");
+  menu.classList.remove("menu__hidden");
+};
+
+const closeMenu = () => {
+  const menu = document.getElementById("menu");
+  menu.classList.add("menu__hidden");
+};
+
+const openDropdown = () => {
+  const dropdown = document.getElementById("dropdown");
+  dropdown.classList.remove("dropdown__hidden");
+};
+const closeDropdown = () => {
+  const dropdown = document.getElementById("dropdown");
+  dropdown.classList.add("dropdown__hidden");
+};
+
+openMenuButton.addEventListener("click", openMenu);
+closeMenuButton.addEventListener("click", closeMenu);
+dropdownButton.addEventListener("mouseover", openDropdown);
+dropdownButton.addEventListener("mouseout", closeDropdown);
