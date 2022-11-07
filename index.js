ac8c1529c768b7f4e4a4606e1f104cb08e9a20e4
icarobteles <eng.icaroteles@gmail.com>
2022-11-06 22:26:05 -0300

    feat: :sparkles: add sections and footer

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-/* const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: false,
+  slidesPerView: 1,
+
+  breakpoints: {
+    960: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+
+    1320: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
 
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".carousell--navigation__next",
+    prevEl: ".carousell--navigation__prev",
   },
 });
- */
